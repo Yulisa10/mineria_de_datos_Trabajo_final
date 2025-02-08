d10dc76ec817fd63deb31ef00e2d9d69f50783d5
JuanPablo9999 <junorenal@unal.edu.co>
2025-02-08 08:48:07 -0500

    Update TrabajoFinal.py

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -57,13 +57,10 @@ X, y, scaler = preprocess_data(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Entrenar modelo MLP
-def train_mlp():
-    model = Sequential()
-    model.add(Dense(32, input_shape=(X_train.shape[1],), activation='relu'))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=50, batch_size=500, verbose=0)
+def load_model():
+      filename = 'keras_model.pkl.gz'
+    with gzip.open(filename, 'rb') as f:
+        model = pickle.load(f)
     return model
 
 # Mostrar contenido basado en la selección
@@ -216,21 +213,11 @@ elif seccion == "Conclusión: Selección del Mejor Modelo":
 elif seccion == "Modelo XGBoost":
     st.subheader("Modelo planteado con XGBoost")
 
-    # Simulación de datos (sustituye esto con tus datos reales)
-    X = df.drop(columns=["Occupancy"], errors='ignore')
-    y = df["Occupancy"]
-
-    # Preprocesamiento de datos
-    scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-
-    # Construcción del modelo XGBoost
-    model = XGBClassifier(enable_categorical=True, random_state=42)
-
-    # Entrenamiento del modelo
-    st.write("Entrenando el modelo XGBoost, por favor espera...")
-    model.fit(X_train, y_train)
+    def load_model():
+      filename = 'xgb_model.pkl.gz'
+      with gzip.open(filename, 'rb') as f:
+        model = pickle.load(f)
+      return model
 
     # Predicciones y evaluación del modelo
     y_pred = model.predict(X_test)
